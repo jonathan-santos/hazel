dade8a139e74b851eb456873d332ad3b3f66dba0
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2025-02-18 10:40:31 -0300

    add Input Manager

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,8 @@
 #include "Hazel/Log.h"
 #include <Glad/glad.h>
 
+#include "Input.h"
+
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -60,6 +62,9 @@ namespace Hazel {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
